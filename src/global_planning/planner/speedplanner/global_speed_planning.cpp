c99020ede42d7a12eb1cf66462fb5e7fd41fde47
yanyifeng <yanyifeng@wicri.org>
2024-12-03 12:43:25 +0800

    修复误删除曲率限速的功能

--- src/global_planning/planner/speedplanner/global_speed_planning.cpp
+++ src/global_planning/planner/speedplanner/global_speed_planning.cpp
@@ -924,12 +924,12 @@ void GlobalSpeedPlanning::ReplanPointMaxSpeed() {
 
 
     // 曲率限速
-    // iter = trajectory_points.begin();
-    // for (; iter != trajectory_points.end(); iter++) {
-    //     if (iter->speed_limit > sqrt(0.4 / fabs(iter->curvature))) {
-    //         iter->speed_limit = sqrt(0.4 / fabs(iter->curvature));
-    //     }
-    // }
+    iter = trajectory_points.begin();
+    for (; iter != trajectory_points.end(); iter++) {
+        if (iter->speed_limit > sqrt(0.4 / fabs(iter->curvature))) {
+            iter->speed_limit = sqrt(0.4 / fabs(iter->curvature));
+        }
+    }
 
     threadLogger_->info("限速设置--在这里打印路径点限速信息");
     for (size_t index = 0; index < trajectory_points.size(); index++) {
