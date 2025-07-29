b140e53deb6876f9cda341be45fd8af3e05c40f9
liulvfan <liulvfan@wicri.org>
2025-07-29 10:03:06 +0800

    增加了文件

---
+++ src/global_planning/load_area_planning/load_area_planning.h
@@ -0,0 +1,46 @@
+#ifndef LOAD_AREA_PLANNING
+#define LOAD_AREA_PLANNING
+#include "fitting_path_generate.h"
+#include "wait_point_generate.h"
+namespace LoadAreaPlanning {
+class LoadAreaPlanning {
+  private:
+    double max_curve_length_;                 // 圆弧直线倒车最大曲线长度
+    double min_curve_length_;                 // 圆弧直线倒车最小曲线长度
+    double delta_curve_length_;               // 圆弧直线倒车曲线长度采样间距
+    double wheel_base_length_;                // 车辆轴距
+    double center2side_;                      // 车辆宽度
+    double max_straight_length_;              // 圆弧直线倒车最大直线长度
+    double min_straight_length_;              // 圆弧直线倒车最小直线长度
+    double delta_straight_length_;            // 圆弧直线倒车直线采样间距
+    double max_steering_angle_;               // 圆弧直线倒车最大转向角
+    double min_steering_angle_;               // 圆弧直线倒车最小转向角
+    double delta_steering_angle_;             // 圆弧直线倒车转角采样间距
+    double standard_steering_angle_;          // 标准倒车转角
+    double weight_length_;                    // 圆弧直线倒车长度评分权重
+    double weight_curve_;                     // 圆弧直线倒车转角评分权重
+    double load_path_straight_length_weight_; // 前往装载点路径直线长度权重
+    double load_path_curvature_weight_;       // 前往装载点路径曲率权重
+    double out_put_path_dense_;               // 输出路径密度
+    double search_range_;                     // 寻找周围路径范围
+    double jump_dense_;                       // 在目标路径上取点的密度
+    double length_weight_;                    // 前进路径直线长度权重
+    double critical_length_;                  // 前进路径长度标准值
+    double curvature_weight_;                 // 前进路径曲率权重
+    double min_straight_length_depart_;       // 驶离路径最短直线长度
+    double max_straight_length_depart_;       // 驶离路径最长直线长度
+    double delta_straight_length_depart_;     // 驶离路径长度采样间距
+    double min_straight_length_wait_;         // 前往等待点路径直线长度最大值
+    double max_straight_length_wait_;         // 前往等待点路径直线长度最小值
+    double delta_straight_length_wait_;       // 前往等待点路径直线长度采样间距
+    double min_straight_length_load_;         // 前往装载点路径直线长度最小值
+    double max_straight_length_load_;         // 前往装载点路径直线长度最大值
+    double delta_straight_length_load_;       // 前往装载点路径直线长度采样间距
+
+  public:
+    ~LoadAreaPlanning();
+    std::tuple<bool, GlobalPlanning::Point, GlobalPlanning::Path, GlobalPlanning::Path, GlobalPlanning::Path> LoadAreaPlanningInterface(int planning_mode, const GlobalPlanning::Point& wait_point, const GlobalPlanning::Point& load_poit, const GlobalPlanning::Path& in_path, GlobalPlanning::Path out_path, GlobalPlanning::CollisonCheck collision_checker);
+};
+} // namespace LoadAreaPlanning
+
+#endif
---
+++ src/global_planning/load_area_planning/wait_point_generate.h
@@ -0,0 +1,51 @@
+#ifndef WAIT_POINT_GENERATE
+#define WAIT_POINT_GENERATE
+#include "fitting_path_generate.h"
+namespace WaitPointGenerate {
+struct arc_sample_point {
+    GlobalPlanning::Point path_point;
+    double                steering;
+    double                arc_length;
+    double                grade;
+};
+
+class WaitPointGenerator {
+  private:
+    double                   max_curve_length_;
+    double                   min_curve_length_;
+    double                   delta_curve_length_;
+    double                   wheel_base_length_;
+    double                   max_straight_length_;
+    double                   min_straight_length_;
+    double                   delta_straight_length_;
+    double                   max_steering_angle_;
+    double                   min_steering_angle_;
+    double                   delta_steering_angle_;
+    double                   standard_steering_angle_;
+    double                   weight_length_;
+    double                   weight_curve_;
+    double                   out_put_path_dense_;
+    GlobalPlanning::Path     CalArcPath(double steer, double wheelbase, double arc_total_length, const GlobalPlanning::Point& straight_end);
+    double                   DeterminateStraightLength(const GlobalPlanning::Point& load_point, GlobalPlanning::CollisonCheck& collision_checker);
+    vector<arc_sample_point> SampleCandiPoints(GlobalPlanning::CollisonCheck& collision_checker, double max_steering, double min_steering, const GlobalPlanning::Point& start_point);
+    double                   distanceSquared(const GlobalPlanning::Point& point, const GlobalPlanning::Point& other) const;
+    bool                     doSegmentsIntersect(const GlobalPlanning::Point& p1, const GlobalPlanning::Point& p2, const GlobalPlanning::Point& q1, const GlobalPlanning::Point& q2);
+    bool                     arePathsCrossing(const std::vector<GlobalPlanning::Point>& path1, const std::vector<GlobalPlanning::Point>& path2, double distanceThreshold = 5.0);
+    GlobalPlanning::Path     GenerateStraitLine(const GlobalPlanning::Point& start, const GlobalPlanning::Point& end);
+    void                     CalculateWaitPointGrade(const GlobalPlanning::Path& load_path);
+    GlobalPlanning::Point    FindNearestPoint(double x, double y, const GlobalPlanning::Path& path);
+    double                   CalculateGradeAboutLoadPath(const arc_sample_point& sample_point, const GlobalPlanning::Point& closest_point);
+    double                   NormalizeAngle(double angle);
+    double                   LinearScore(double diff, double target_diff, double tolerance);
+
+  public:
+    GlobalPlanning::Path GenerateWaitPointInterface(const GlobalPlanning::Point& load_point, const GlobalPlanning::Path& depart_path, GlobalPlanning::CollisonCheck& collision_checker, FittingPathGenerate::FittingPathGenerator& fitting_path_generator, const GlobalPlanning::Path& in_path);
+    WaitPointGenerator(double max_curve_length, double min_curve_length, double delta_curve_length, double wheel_base_length, double max_straight_length, double min_straight_length, double delta_straight_length, double max_steering_angle, double min_steering_angle, double delta_steering_angle, double standard_steering_angle, double weight_length, double weight_curve, double out_put_path_dense);
+    vector<arc_sample_point> wait_point_sample_;
+
+
+    ~WaitPointGenerator();
+};
+} // namespace WaitPointGenerate
+
+#endif
